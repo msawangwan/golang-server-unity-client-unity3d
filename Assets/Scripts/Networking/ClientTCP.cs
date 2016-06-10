4db796cab23a086a6b28c5481e0b12c00232c7d0
msawangwan <misha.sawangwan@gmail.com>
2016-06-09 18:06:45 -0700

    some unity scene changes

--- Assets/Scripts/Networking/ClientTCP.cs
+++ Assets/Scripts/Networking/ClientTCP.cs
@@ -1,115 +1,197 @@
 ﻿using UnityEngine;
-using System.Threading;
+using System;
 using System.Text;
-using System.IO;
 using System.Net;
-using System.Net.Sockets;  
+using System.Net.Sockets;
+
+/// <summary>
+/// Asynchronous TCP Client.
+/// </summary>
+
+public enum ClientConnState {
+    None = 0,
+    Connected = 1,
+    Disconnected = 2,
+    Idle = 3,
+    Timeout = 4,
+}
 
 public class ClientTCP {
+    /* Maintains socket state between socket reads. */
+    sealed class SocketState {
+        public Socket WorkSocket { get; set; }
+        public Packet DataFrame { get; set; }
+
+        public int TotalBytesRead { get; set; }
+        public byte[] Buffer { get; private set; }
+
+        private const int bufferSize = 16;
+
+        public SocketState() {
+            Buffer = new byte[bufferSize];
+            DataFrame = new Packet ( );
+
+            TotalBytesRead = 0;
+        }
+    }
+
+    /* Represents a data frame. */
+    sealed class Packet {
+        public byte[] Buffer { get; private set; }
+
+        private const int packetSize = 4096;
+        private int bufferPosition = 0;
+
+        public Packet() {
+            Buffer = new byte[packetSize];
+        }
+
+        public void AddData(byte[] buffer, int bufferOffset) {
+            System.Buffer.BlockCopy ( buffer, 0, Buffer, bufferPosition, buffer.Length );
+            bufferPosition += bufferOffset;
+        }
+    }
+
+    private ClientConnState connectionState = ClientConnState.None;
+
     private const string serverAddr = "127.0.0.1";
     private const int serverPort = 9080;
 
-    private static Socket serverConn;
+    private static Socket serverSocket;
     private static IPAddress serverIP;
     private static IPEndPoint serverEndpoint;
 
-    private NetworkStream socketStream;
-
-    private int connRetryAttempts = 0;
-    private bool isConnected = false;
-    private bool hasFailedToConnect = false;
-
     /* Default constructor */
-    public ClientTCP () {
+    public ClientTCP ( ) {
         serverIP = IPAddress.Parse ( serverAddr );
-        serverEndpoint = new IPEndPoint ( serverIP , serverPort );
+        serverEndpoint = new IPEndPoint ( serverIP, serverPort );
     }
 
-    public bool EstablishConnection ( ) {
-        serverConn = OpenTCPSocket ( );
+    public void Connect ( ) {
+        serverSocket = OpenTCPSocket ( );
+        serverSocket.BeginConnect ( serverEndpoint, new AsyncCallback ( ConnectCallback ), null );
+    }
+
+    public void Disconnect() {
+        DisconnectCallback ( );
+    }
+
+    public void Send ( string data ) {
+        try {
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            serverSocket.BeginSend ( buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback ( SendCallback ), null );
+        } catch ( SocketException se ) {
+            Debug.Log ( se );
+        } catch ( Exception e ) {
+            Debug.Log ( e );
+        }
+    }
+
+    public void Listen ( ) {
+        try {
+            SocketState clientState = new SocketState();
+            clientState.WorkSocket = serverSocket;
+
+            serverSocket.BeginReceive ( clientState.Buffer, 0, clientState.Buffer.Length, SocketFlags.None, new AsyncCallback ( RecvCallback ), clientState );
+        } catch ( SocketException se ) {
+            Debug.Log ( se );
+        } catch ( Exception e ) {
+            Debug.Log ( e );
+        }
+    }
+
+    private void ConnectCallback ( IAsyncResult ar ) {
+        try {
+            serverSocket.EndConnect ( ar );
+            connectionState = ClientConnState.Connected;
+        } catch ( Exception e ) {
+            Debug.Log ( e );
+        }
+    }
+
+    private void DisconnectCallback ( ) {
+        try {
+            serverSocket.Close ( );
+            connectionState = ClientConnState.Disconnected;
+        } catch ( Exception e ) {
+            Debug.Log ( "[Client:DisconnectCallback] Error on disconnect: " + e );
+        }
+    }
+
+    private void SendCallback ( IAsyncResult ar ) {
+        try {
+            serverSocket.EndSend ( ar );
+        } catch ( Exception e ) {
+            Debug.Log ( e );
+        }
+    }
+
+    private void RecvCallback ( IAsyncResult ar ) {
+        SocketState socketState = (SocketState) ar.AsyncState;
 
         try {
-            Debug.Log ( "Establishing connection to remote server ... " );
-            serverConn.Connect ( serverEndpoint );
-        } catch {
-            Debug.Log ( "Error, unable to establish a connection with the server, retrying ... attempt # " + connRetryAttempts );
+            int bytesRead = socketState.WorkSocket.EndReceive(ar); // read bytes from strem
+            bool isDataFrame = false;
 
-            if ( connRetryAttempts > 3 ) {
-                Debug.Log ( "Failed to connect after " + connRetryAttempts + " retry attempts, terminating." );
-                connRetryAttempts = 0;
-                return false;
-            }
+            socketState.TotalBytesRead += bytesRead;
 
-            ++connRetryAttempts;
-            EstablishConnection ( );
-        }
+            if ( bytesRead > 0 ) {
+                int bufferSize = bytesRead;
+                string currentBuffer = Encoding.ASCII.GetString ( socketState.Buffer );
 
-        socketStream = new NetworkStream ( serverConn );
+                for ( int i = 0; i < currentBuffer.Length; i++ ) { // read through the buffer and look for delimiting char, '\n'
+                    if ( currentBuffer[i] == '\n' ) {
+                        isDataFrame = true;
+                        bufferSize = i + 1;
+                        break;
+                    }
+                }
 
-        return true;
-    }
+                if ( isDataFrame ) { // if we reached a delimiting char, copy any remaining bytes before into the current DataFrame
+                    byte[] temp = new byte[bufferSize];
 
-    public void CloseConnection( ) {
-        socketStream.Close ( );
-        serverConn.Shutdown ( SocketShutdown.Both ); // try with this commented out to see what effects it has
-        serverConn.Close ( );
-
-        Debug.Log ( "Disconnected from server." );
-    }
-
-    public string SendAndReceiveData ( string data ) {
-        byte[] bufferedData = Encoding.ASCII.GetBytes ( data );
-
-        WriteStream ( socketStream, bufferedData );
-        byte[] serverReply = ReadStream(socketStream);
-
-        return Encoding.ASCII.GetString ( serverReply );
-    }
-
-    private Socket OpenTCPSocket() {
-        return new Socket ( AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp );
-    }
-
-    private static void WriteStream(NetworkStream stream, byte[] bufferedData) {
-        if ( stream.CanWrite ) {
-            stream.Write ( bufferedData, 0, bufferedData.Length );
-            stream.Flush ( );
-        }
-    }
-
-    private static byte[] ReadStream(NetworkStream stream) {
-        if (stream.CanRead) {
-            int readCount = 0;
-            int startIndex = 0;
-            int totalBytesRead = 0;
-
-            byte[] buffer = new byte[4096];
-            byte[] tmpBuffer = new byte[32];
-
-            using (MemoryStream writer = new MemoryStream()) {
-                do {
-                    readCount++;
-                    
-                    int numBytesRead = stream.Read(tmpBuffer, 0, tmpBuffer.Length);
-                    totalBytesRead += numBytesRead;
-
-                    Debug.Log ( numBytesRead + " " + readCount + " " + totalBytesRead + " " + startIndex);
-                    if ( numBytesRead <= 0 ) {
-                        if ( stream.ReadByte() == -1 ) {
-                            break;
-                        }
+                    for ( int i = 0; i < bufferSize; i++ ) {
+                        temp[i] = socketState.Buffer[i];
                     }
 
-                    writer.Write ( tmpBuffer, 0, numBytesRead ); // write to the tmpBuffer
-                    System.Buffer.BlockCopy ( tmpBuffer, 0, buffer, startIndex, numBytesRead ); // copy tmpBuffer to buffer
+                    socketState.DataFrame.AddData ( temp, bufferSize );
+                } else { // else just copy the entire buffer
+                    socketState.DataFrame.AddData ( socketState.Buffer, bufferSize );
+                }
 
-                    startIndex = totalBytesRead;
-                } while ( stream.DataAvailable );
+                Debug.Log ( Encoding.ASCII.GetString ( socketState.DataFrame.Buffer ) ); // print for debug
 
-                writer.Close ( );
-                return buffer;
+                if ( isDataFrame ) { // if we reached a delimiting char, reset client state and copy any remaining bytes from the previous buffer state into new buffer state
+                    int bufferRemainder = socketState.Buffer.Length - bufferSize;
+                    byte[] temp = new byte[bufferRemainder];
+
+                    for ( int i = 0; i < bufferRemainder; i++ ) {
+                        temp[i] = socketState.Buffer[bufferSize + i];
+                    }
+
+                    // SEND DATAFRAME OFF ...
+
+                    Debug.Log ( "[Client:RecvCallback] <EOM> New DataFrame <EOM>" );
+                    socketState = new SocketState ( );
+                    socketState.WorkSocket = serverSocket;
+
+                    socketState.DataFrame.AddData ( temp, bufferRemainder );
+                }
             }
+
+            if ( bytesRead == 0 ) { // if no bytes read, the connection is over, todo: handle
+                Debug.Log ( "[Client:RecvCallback] No Bytes Read" );
+            } else { // else keep listening for more
+                socketState.WorkSocket.BeginReceive ( socketState.Buffer, 0, socketState.Buffer.Length, 0, new AsyncCallback ( RecvCallback ), socketState );
+            }
+        } catch ( ObjectDisposedException ode ) {
+            Debug.Log ( ode );
+        } catch ( Exception e ) {
+            Debug.Log ( e );
         }
-        return new byte[0];
+    }
+
+    private static Socket OpenTCPSocket ( ) {
+        return new Socket ( AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp );
     }
 }
--- Assets/Scripts/Networking/NetworkManager.cs
+++ Assets/Scripts/Networking/NetworkManager.cs
@@ -2,39 +2,17 @@
 using System.Collections;
 
 public class NetworkManager : MonoBehaviour {
-    private ClientTCP clientConnection;
-    private ClientTCP_async clientConnection_async;
+    private ClientTCP clientConnection_async;
 
 	void Start () {
-        clientConnection = new ClientTCP ( );
-        clientConnection_async = new ClientTCP_async ( );
-
+        clientConnection_async = new ClientTCP ( );
     }
-
-	void Update () {
-	
-	}
 
     bool isInitialConnection = true;
     bool hasConnected = false;
 
-    public void Test_SendDataToServer() {
-        if ( isInitialConnection ) {
-            hasConnected = clientConnection.EstablishConnection ( );
-            if ( hasConnected ) {
-                isInitialConnection = false;
-            }
-        }
-
-        if ( hasConnected ) {
-            string serverResponse = clientConnection.SendAndReceiveData ( "tessssssticles spectacles wallets and watch" );
-            Debug.Log ( "Response From Server: " + serverResponse );
-        }       
-    }
-
     public void Test_CloseConnection() {
         if (hasConnected) {
-            clientConnection.CloseConnection ( );
             isInitialConnection = true;
             hasConnected = false;
         }
